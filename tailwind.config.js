783bd0be353e4856eb66299abadd67e0c9aac5b3
joao <joao.campos@unemat.br>
2022-08-21 15:23:40 -0400

    adiciona cores personalizadas nas configurações do tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,23 @@ module.exports = {
                 sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors:{
+          bg_login: '#A7C0CD80',
+          primary: '#004BA0',
+          white:'#fff',
+          blue1: {
+            "50": "#aac2ce",
+            "100": "#a0b8c4",
+            "200": "#96aeba",
+            "300": "#8ca4b0",
+            "400": "#829aa6",
+            "500": "#78909c",
+            "600": "#6e8692",
+            "700": "#647c88",
+            "800": "#5a727e",
+            "900": "#506874"
+          }
+        }
     },
 
     plugins: [require('@tailwindcss/forms')],
