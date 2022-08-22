431c9a6843a7a439bf75da300cbe7cfdb4191ae5
joao <joao.campos@unemat.br>
2022-08-22 00:39:33 -0400

    feat: adiciona nova cor nas configurações do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,38 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+                sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
             },
         },
-        colors:{
-          bg_login: '#A7C0CD80',
-          primary: '#004BA0',
-          white:'#fff',
-          blue1: {
-            "50": "#aac2ce",
-            "100": "#a0b8c4",
-            "200": "#96aeba",
-            "300": "#8ca4b0",
-            "400": "#829aa6",
-            "500": "#78909c",
-            "600": "#6e8692",
-            "700": "#647c88",
-            "800": "#5a727e",
-            "900": "#506874"
-          }
-        }
+        colors: {
+            bg_login: "#A7C0CD80",
+            primary: "#004BA0",
+            white: "#fff",
+            blue1: {
+                50: "#aac2ce",
+                100: "#a0b8c4",
+                200: "#96aeba",
+                300: "#8ca4b0",
+                400: "#829aa6",
+                500: "#78909c",
+                600: "#6e8692",
+                700: "#647c88",
+                800: "#5a727e",
+                900: "#506874",
+            },
+            blue2: "#4B636E",
+        },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
