97a43a0a248d5167f7a406bd844cf757202acbc4
joao <joao.campos@unemat.br>
2022-08-22 00:38:16 -0400

    refactor: altera textos e espaçamento na view de login e no componente 'auth-card'

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,10 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+<div class="bg-bg_login min-h-screen flex flex-col sm:justify-center items-center  sm:pt-0 bg-gray-100">
+    <div class="w-full lg:max-w-md xl:max-w-lg sm:max-w-md m-auto mx-4 px-8 py-3 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="p-2">
+            {{ $logo }}
+        </div>
+        <div class="mt-8 p-2" >
+            {{ $slot }}
+        </div>
     </div>
 </div>
