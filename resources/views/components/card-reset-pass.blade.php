d7b0ac4f57c68120a1b6204b07e956dd74001b8a
joao <joao.campos@unemat.br>
2022-08-22 11:18:32 -0400

    feat: cria componente card-reset-pass para as páginas de auth que não usam logo

---
+++ resources/views/components/card-reset-pass.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-bg_login min-h-screen flex flex-col sm:justify-center items-center  sm:pt-0 bg-gray-100">
+    <div class="w-full lg:max-w-md xl:max-w-lg sm:max-w-md m-auto mx-4 px-8 py-3 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="mt-8 p-2" >
+            {{ $slot }}
+        </div>
+    </div>
+</div>
